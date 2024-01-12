f1dd526665eba2b99b8ab65666900317c3e16501
Rdebora <riosdeborasabrina@gmail.com>
2024-01-12 16:24:10 -0300

    primeros pasos

---
+++ holaMundo.cs
@@ -0,0 +1,27 @@
+﻿// contexto de ejecucion para importar modulo
+using System;
+
+// clases
+// Gracias a lo de la linea 8 se genera el punto de ejecucion.
+class HelloWord
+{
+    static void Main(string[] args)
+    {
+        // imprime en la consola un mensaje es el primo de console.log()
+        Console.WriteLine("Hello, MILTONNN!");
+
+
+
+
+        string miSaludo = "hola como estas?";
+        miSaludo = "tod bien?";
+        Console.WriteLine(miSaludo);
+    }
+
+
+
+
+};
+
+
+
